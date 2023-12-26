bd2fbe50a8629b888ed0e80ce5de6dc3bb086380
Fernando Ayach <ferayach@gmail.com>
2023-12-26 09:24:13 -0400

    [NEW] Finish first course

--- escola/views.py
+++ escola/views.py
@@ -1,11 +1,42 @@
-from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from serializer import AlunoSerializer, CursoSerializer
+from rest_framework import viewsets, generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from escola.models import Aluno, Curso, Matricula
+
+from escola.serializer import \
+AlunoSerializer, CursoSerializer, MatriculaSerializer, \
+ListaMatriculasAlunoSerializer, ListaAlunosCursoSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
-class CursoViewSet(viewsets.ModelViewSet):
+class CursosViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaAlunosCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosCursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
